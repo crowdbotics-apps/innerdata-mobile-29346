77acdc633b534baceca9d46f8a076328607a394f
Crowdbotics <team@crowdbotics.com>
2021-08-02 21:25:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "innerdata_mobile_29346Identifier",
+  url: "https://innerdata_mobile_29346Identifier.botics.co",
+  api: "https://innerdata_mobile_29346Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
